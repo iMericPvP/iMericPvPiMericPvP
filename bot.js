dd414326b57094b9439095df67684713f94eac34
iMericPvP <40717146+iMericPvP@users.noreply.github.com>
2018-06-30 17:05:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,7 +38,26 @@ message.channel.sendEmbed(cat);
     }
 });
 
+const superagent = require("superagent");
 
+client.on('message', async message => {
+  if (message.content.startsWith(prefix + 'hu')) {
+       let args = message.content.split(" ").slice(1);
+    let hugUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!hugUser) return message.channel.send("Make sure you mention someone!");
+    const {body} = await superagent
+    .get(`https://nekos.life/api/v2/img/hug`);
+    let hugEmbed = new Discord.RichEmbed()
+    .setTitle("Hug! c:")
+    .setDescription(`**${message.author.username}** hugged **${message.mentions.users.first().username}**!`)
+    .setImage(body.url)
+    .setColor("RANDOM")
+    .setFooter("Bot Version: 0.0.4", client.user.displayAvatarURL);
+
+    message.channel.send(hugEmbed)
+
+}
+});
 
 
 
