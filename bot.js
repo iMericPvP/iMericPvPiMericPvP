6a826be5dec2af002c2ec828e0cd014af39a0188
DKSDAI3sKA <41757098+DKSDAI3sKA@users.noreply.github.com>
2018-08-17 02:50:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '/'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`/help | /invite `,"http://twitch.tv/NoobBot")
+client.user.setGame(`/help | /invite | ${client.guilds.size} `,"http://twitch.tv/NoobBot")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
